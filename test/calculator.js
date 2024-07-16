befce1297c330b3e0fffd8106afcf951d26a5050
kungfooman <lama12345@gmail.com>
2024-07-16 11:44:45 +0200

    Add /test/

---
+++ test/calculator.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} a - First number.
+ * @param {number} b - Second number.
+ * @returns 
+ */
+function add(a, b) {
+  return a + b;
+}
+export {add};
--- test/test-workerframe-script.mjs
+++ test/test-workerframe-script.mjs
@@ -1,15 +1,14 @@
-import {add} from 'calculator';
 self.onmessage = (e) => {
   console.log("self.onmessage e", e);
   const {data} = e;
   self.postMessage({
-    message: `Worker received: ${data} and add(1, 2) is ${add(1, 2)}`
+    message: `Worker received: ${data}`
   });
 };
 self.addEventListener('message', (e) => {
   console.log("self.addEventListener('message', e)", e);
   const {data} = e;
   self.postMessage({
-    message: `Worker received: ${data} and add(1, 2) is ${add(1, 2)}`
+    message: `Worker received: ${data}`
   });
 });
